eb6b3dab0d6e583fc94babc28fb5cd3770d73a0e
UkandieSunday <ukandieudo202@gmail.com>
2024-01-17 17:44:28 +0100

    added pagination

--- client/src/component/BlogCards/BlogCards.jsx
+++ client/src/component/BlogCards/BlogCards.jsx
@@ -1,19 +1,83 @@
 import "./BlogCards.css";
-import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
-const BlogCards = ({ blogs }) => {
-  const filteredBlogs = blogs;
-  console.log(filteredBlogs);
+const BlogCards = ({
+  blogs,
+  pageSize,
+  currentPage,
+  category,
+  handlePageChange,
+}) => {
+  const filteredBlogs = blogs
+    .filter((blog) => !category || blog.category === category)
+    .slice((currentPage - 1) * pageSize, currentPage * pageSize);
+
+  // Pagination
+  const totalPage = Math.ceil(blogs.length / pageSize);
+  const renderPagination = () => {
+    const pageNumbers = Array.from({ length: totalPage }, (_, i) => i + 1);
+    return pageNumbers.map((pageNumber) => (
+      <li key={pageNumber}>
+        <a
+          className={currentPage === pageNumber ? "active-page" : ""}
+          href="#"
+          onClick={() => handlePageChange(pageNumber)}
+        >
+          {pageNumber}
+        </a>
+      </li>
+    ));
+  };
+
   return (
-    <div className="blogCards">
-      {filteredBlogs.map((blog) => (
-        <div className="blogs-wrapper">
-          <div>
-            <img src={blog.image} alt="" />
+    <div className="blog-cards">
+      <div className="card-container">
+        {filteredBlogs?.map((blog) => (
+          <div key={blog.id} className="card-wrapper">
+            <div className="img-container">
+              <img src={blog?.image} alt="" />
+            </div>
+            <div className="items-wrapper">
+              <h4>{blog?.title}</h4>
+              <p>{blog?.content}</p>
+              {/* <img src={blog?.authorPic} alt="" /> */}
+              <span>{blog?.author}</span>
+              <span>{blog?.published_date}</span>
+            </div>
           </div>
-          <h4>{blog.content}</h4>
-        </div>
-      ))}
+        ))}
+      </div>
+
+      {/* display pagination */}
+      <div className="pagination">
+        <p>
+          page {currentPage} of {totalPage} pages
+        </p>
+        <ul className="pagination-container">
+          <li>
+            <button
+              className="btns btns1"
+              onClick={() => handlePageChange(currentPage - 1)}
+              disabled={currentPage === 1}
+            >
+              <IoIosArrowBack />
+            </button>
+          </li>
+          <div className="pageNumber-container">
+            <div className="pageNumber-wrapper">{renderPagination()}</div>
+          </div>
+          <li>
+            <button
+              className="btns btns2"
+              onClick={() => handlePageChange(currentPage + 1)}
+              disabled={currentPage === totalPage}
+            >
+              <IoIosArrowForward />
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
--- client/src/component/Blogs/Blogs.jsx
+++ client/src/component/Blogs/Blogs.jsx
@@ -4,18 +4,44 @@ import axios from "axios";
 import BlogCards from "../BlogCards/BlogCards";
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
-  const fetchBlogs = async () => {
-    const url = "https://modern-blog-website-sever.vercel.app/blogs";
-    const data = await axios.get(url).then((res) => res.data);
-    setBlogs(data);
-  };
+  const [currentPage, setCurrentPage] = useState(1);
+  const [category, setCategory] = useState("");
+  let pageSize = 8;
   useEffect(() => {
-    fetchBlogs();
-  }, []);
+    const fetchBlogs = async () => {
+      const url = `https://modern-blog-website-sever.vercel.app/blogs?page=${currentPage}&limit=${pageSize}&category=${category}`;
 
+      const data = await axios.get(url).then((res) => res.data.blogs);
+      setBlogs(data);
+    };
+    fetchBlogs();
+  }, [currentPage, pageSize, category]);
+  console.log(blogs);
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+  const handleCategoryChange = (category) => {
+    setCategory(category);
+    setCurrentPage(1);
+  };
   return (
-    <div className="blogs">
-      <BlogCards blogs={blogs} />
+    <div>
+      <div className="blogs">
+        <div className="blogs-left">
+          <BlogCards
+            blogs={blogs}
+            // handleCategoryChange={handleCategoryChange}
+            category={category}
+            pageSize={pageSize}
+            currentPage={currentPage}
+            handlePageChange={handlePageChange}
+          />
+        </div>
+        <div className="blogs-right">
+          <h2>Explore popular and current news </h2>
+          <div className="category">{/* call category component */}</div>
+        </div>
+      </div>
     </div>
   );
 };
